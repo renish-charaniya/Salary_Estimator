d95e4386e0d750eee9a0a04c547cd644d83c56fd
Renish Charaniya <renish9923@gmail.com>
2020-06-02 21:55:09 +0530

    data ceaning script

--- data_collection.py
+++ data_collection.py
@@ -1,6 +1,6 @@
 import glassdoor_scraper as gs
 import pandas as pd 
 path = "/usr/local/share/chromedriver"
-df = gs.get_jobs('India','data scientist',5, False, path, 15)
+df = gs.get_jobs('India','data scientist',50, False, path, 15)
 df.to_csv('glassdoor_jobs.csv', index = False)
 
